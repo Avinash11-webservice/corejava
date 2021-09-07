d4b2b15d2657de66be5e225256ed4dcf0de0259e
Avinash11-webservice <89975369+Avinash11-webservice@users.noreply.github.com>
2021-09-07 14:24:05 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Student {
+	      int rollno=2;
+	      String name="John";
+	      public static void main(String[] args)
+	       {
+	     
+	              Student stud1=new Student();
+	             System.out.println(stud1.name);
+	             System.out.println(stud1.rollno);
+	         }
+	
+
+}
