191c6440d4c78f197bb09df2c5453af19f0890ca
Avinash11-webservice <89975369+Avinash11-webservice@users.noreply.github.com>
2021-09-11 11:11:23 +0530

    Add files via upload

---
+++ sep10/stringdemo/StringDemo.java
@@ -0,0 +1,22 @@
+package com.exception;
+
+public class StringDemo {
+	public static void main(String[]args) {
+		
+	 String name ="Avinash";
+     System.out.println(" length of the string  "+name.length());
+
+
+     String uppercasedemo ="AVINASH";
+     System.out.println("  string result  "+uppercasedemo.toLowerCase());
+
+     String lowercasedemo ="avinash";
+     System.out.println("  string result  "+uppercasedemo.toUpperCase());
+
+     char ast = lowercasedemo.charAt(5);
+
+     System.out.println("  postion of char  "+ast);
+    
+
+}
+}
