720bef4fa1a4eabde93e2e0584b589f07494d2cb
Avinash11-webservice <89975369+Avinash11-webservice@users.noreply.github.com>
2021-09-21 11:01:40 +0530

    Add files via upload

---
+++ JDBC/Demo.java
@@ -0,0 +1,36 @@
+package com.jdbc;
+
+	import java.sql.Connection;
+	import java.sql.DriverManager;
+	 
+	public class Demo {
+	    public static void main(String ar[]) {
+	 
+	        @SuppressWarnings("unused")
+			Connection connection = null;
+	        final String username = " sa";
+	        final String password = "";
+	        final String jdbcurl =  "jdbc:h2:~/test";
+	 
+	        // String dbURL = "jdbc:sqlserver://localhost\\sqlexpress";
+	        // String user = "sa";
+	        // String pass = "";
+	 
+	        try {
+	 
+	            Class.forName("org.h2.Driver"); ///
+	 
+	            connection = DriverManager.getConnection(jdbcurl,username,password);///authentication
+	 
+	            System.out.println("connected...");
+	 
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	 
+	        }
+	 
+	    }
+	 
+	}
+
+
